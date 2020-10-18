87532b58b06671e0a11c9c0f783b97195b906239
mrtrvl <mrt@mrt.ee>
2020-10-18 21:10:47 +0300

    Add comments for endpoints

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Import express and put it into express constant
 const express = require('express');
+// Create express object and put it into app constant
 const app = express();
 
+// Middleware required for receiving body from request object as JSON
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Database mockup
 const users = [
     {
         id: 0,
@@ -21,15 +25,22 @@ const users = [
     }
 ];
 
+// Endpoint for checking if API is alive (response 200 OK means, it is working)
+// GET - ping
+// Required values: none
+// Optional values: none
+// Returns: status 200 - OK and { success: true } message
 app.get('/api/ping', (req, res) => {
     res.status(200).json({
         success: true
     });
 });
 
+// Endpoint for getting list of available users
 // GET - users
-// Required: none
-// Optional: none
+// Required values: none
+// Optional values: none
+// Returns: status 200 - OK and list of users in response body
 app.get('/api/users', (req, res) => {
     res.status(200).json({
         success: true,
@@ -37,9 +48,12 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+
+// Endpoint for getting user specified by id
 // GET - users
 // Required: id
 // Optional: none
+// Returns: status 200 - OK and user data in response body
 app.get('/api/users/:id', (req, res) => {
     res.status(200).json({
         success: true,
@@ -47,9 +61,13 @@ app.get('/api/users/:id', (req, res) => {
     });
 });
 
+// Endpoint for creating new user
 // POST - users
-// Required: firstName, lastName, email, password
-// Optional: none
+// Required values: firstName, lastName, email, password
+// Optionalvalues: none
+// Returns:
+//  Success: status 201 - Created and user data in response body
+//  Fail: status 400 - Bad Request and error message in response body
 app.post('/api/users', (req, res) => {
     const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
     const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
@@ -80,9 +98,13 @@ app.post('/api/users', (req, res) => {
     }
 });
 
+// Endpoint for updating user specified by id
 // PUT - users
 // Required: id
 // Optional: firstName, lastName, email, password
+// Returns:
+//  Success: status 200 - OK and user data in response body
+//  Fail: status 400 - Bad Request and error message in response body
 app.put('/api/users', (req, res) => {
     const id = typeof(req.body.id) === 'number' ? req.body.id : false;
     const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
@@ -114,9 +136,13 @@ app.put('/api/users', (req, res) => {
     }
 });
 
+// Endpoint for deleting user specified by id
 // DELETE - users
 // Required: id
 // Optional: none
+// Returns:
+//  Success: status 200 - OK and { success: true } message
+//  Fail: status 400 - Bad Request and error message in response body
 app.delete('/api/users', (req, res) => {
     const id = typeof(req.body.id) === 'number' ? req.body.id : false;
     if(id || id === 0) {
