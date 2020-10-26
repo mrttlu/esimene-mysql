21a91bc84e1d87171c65625e083b75f6ff723474
mrtrvl <mrt@mrt.ee>
2020-10-26 11:41:16 +0200

    Add homeworks endpoints

--- index.js
+++ index.js
@@ -508,6 +508,134 @@ app.delete('/api/subjects', (req, res) => {
     }
 });
 
+// Endpoint for getting list of available homeworks
+// GET - homeworks
+// Required values: none
+// Optional values: none
+// Returns: status 200 - OK and list of homeworks in response body
+app.get('/api/homeworks', (req, res) => {
+    // Return list of homeworks
+    res.status(200).json({
+        success: true,
+        homeworks
+    });
+});
+
+// Endpoint for getting homework specified by id
+// GET - homeworks
+// Required: id
+// Optional: none
+// Returns: status 200 - OK and homework data in response body
+app.get('/api/homeworks/:id', (req, res) => {
+    // Return homework with specified id
+    res.status(200).json({
+        success: true,
+        subjects: homeworks[req.params.id]
+    });
+});
+
+// Endpoint for creating new homework
+// POST - homeworks
+// Required values: description, dueDate, subjectId, userId
+// Optional values: none
+// Returns:
+//  Success: status 201 - Created and homework data in response body
+//  Fail: status 400 - Bad Request and error message in response body
+app.post('/api/homeworks', (req, res) => {
+    // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    const description = typeof(req.body.description) === 'string' && req.body.description.trim().length > 0 ? req.body.description : false;
+    const dueDate = new Date();
+    const subjectId = typeof(req.body.subjectId) === 'number' ? req.body.subjectId : false;
+    const userId = typeof(req.body.userId) === 'number' ? req.body.userId : false;
+    // Check if required data exists
+    if (description && dueDate && (subjectId || subjectId === 0) && (userId || userId === 0)) {
+        // Create new json with user data
+        const newHomework = {
+            id: homeworks.length,
+            description,
+            subjectId,
+            userId
+        };
+        // Add homework to 'database'
+        homeworks.push(newHomework);
+
+        // Return data
+        res.status(201).json({
+            success: true,
+            homework: newHomework
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
+
+// Endpoint for updating homework specified by id
+// PUT - homeworks
+// Required: id
+// Optional: description, dueDate, subjectId
+// Returns:
+//  Success: status 200 - OK and subject data in response body
+//  Fail: status 400 - Bad Request and error message in response body
+app.put('/api/homeworks', (req, res) => {
+    // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    const description = typeof(req.body.description) === 'string' && req.body.description.trim().length > 0 ? req.body.description : false;
+    const dueDate = new Date();
+    const subjectId = typeof(req.body.subjectId) === 'number' ? req.body.subjectId : false;
+    // Check if required data exists
+    if(id || id === 0) {
+        // Check if optional data exists
+        if (description) {
+            // Change user data in 'database'
+            homeworks[id].description = description;
+        }
+        // Check if optional data exists
+        if ((subjectId || subjectId === 0)) {
+            // Change user data in 'database'
+            homeworks[id].subjectId = subjectId;
+        }
+        // Return updated user data
+        res.status(200).json({
+            success: true,
+            homeworks: homeworks[id]
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
+
+// Endpoint for deleting homework specified by id
+// DELETE - homeworks
+// Required: id
+// Optional: none
+// Returns:
+//  Success: status 200 - OK and { success: true } message
+//  Fail: status 400 - Bad Request and error message in response body
+app.delete('/api/homeworks', (req, res) => {
+    // Check if required data exists
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    if(id || id === 0) {
+        homeworks.splice(id, 1);
+        // Return success message
+        res.status(200).json({
+            success: true
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server running');
