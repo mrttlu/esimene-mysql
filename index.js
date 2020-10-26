f16e71560f38f1d3987ed2adc5476b7f618c73d3
mrtrvl <mrt@mrt.ee>
2020-10-26 10:55:55 +0200

    Add lectures endpoints

--- index.js
+++ index.js
@@ -243,6 +243,142 @@ app.delete('/api/users', (req, res) => {
     }
 });
 
+// Endpoint for getting list of available lecturers
+// GET - lecturers
+// Required values: none
+// Optional values: none
+// Returns: status 200 - OK and list of users in response body
+app.get('/api/lecturers', (req, res) => {
+    // Return list of users
+    res.status(200).json({
+        success: true,
+        lecturers
+    });
+});
+
+// Endpoint for getting lecturer specified by id
+// GET - lecturers
+// Required: id
+// Optional: none
+// Returns: status 200 - OK and lecturer data in response body
+app.get('/api/lecturers/:id', (req, res) => {
+    // Return user with specified id
+    res.status(200).json({
+        success: true,
+        lecturers: lecturers[req.params.id]
+    });
+});
+
+// Endpoint for creating new lecturer
+// POST - lecturers
+// Required values: firstName, lastName, email
+// Optionalvalues: none
+// Returns:
+//  Success: status 201 - Created and lecturer data in response body
+//  Fail: status 400 - Bad Request and error message in response body
+app.post('/api/lecturers', (req, res) => {
+    // Check if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+    const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+    const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
+
+    // Check if required data exists
+    if (firstName && lastName && email) {
+        // Create new json with user data
+        const newLecturer = {
+            id: lecturers.length,
+            firstName,
+            lastName,
+            email
+        };
+        // Add lecturer to 'database'
+        lecturers.push(newLecturer);
+
+        // Return data
+        res.status(201).json({
+            success: true,
+            lecturer: newLecturer
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
+
+// Endpoint for updating lecturer specified by id
+// PUT - lecturers
+// Required: id
+// Optional: firstName, lastName, email
+// Returns:
+//  Success: status 200 - OK and lecturer data in response body
+//  Fail: status 400 - Bad Request and error message in response body
+app.put('/api/lecturers', (req, res) => {
+    // Next lines checking if provided data is expected type (typeof) and has length when whitespace is removed (.trim().length)
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+    const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+    const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
+    // Check if required data exists
+    if(id || id === 0) {
+        // Check if optional data exists
+        if (firstName) {
+            // Change user data in 'database'
+            lecturers[id].firstName = firstName;
+        }
+        // Check if optional data exists
+        if (lastName) {
+            // Change user data in 'database'
+            lecturers[id].lastName = lastName;
+        }
+        // Check if optional data exists
+        if (email) {
+            // Change user data in 'database'
+            lecturers[id].email = email;
+        }
+        // Return updated user data
+        res.status(200).json({
+            success: true,
+            lecturer: lecturers[id]
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
+
+// Endpoint for deleting lecturer specified by id
+// DELETE - lecturers
+// Required: id
+// Optional: none
+// Returns:
+//  Success: status 200 - OK and { success: true } message
+//  Fail: status 400 - Bad Request and error message in response body
+app.delete('/api/lecturers', (req, res) => {
+    // Check if required data exists
+    const id = typeof(req.body.id) === 'number' ? req.body.id : false;
+    if(id || id === 0) {
+        lecturers.splice(id, 1);
+        // Return success message
+        res.status(200).json({
+            success: true
+        });
+    } else {
+        // Return error message
+        res.status(400).json({
+            success: false,
+            message: 'Required field(s) missing or invalid'
+        });
+    }
+});
+
+
+
 app.listen(3000, () => {
     console.log('Server running');
 });
