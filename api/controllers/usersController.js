5cd1dcf7fb0129727a9cfbee7aa247eb0486d5ba
Martti Raavel <mrt@tlu.ee>
2020-10-31 12:51:43 +0200

    Update users service and controller

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,21 +1,5 @@
-const users = [
-    {
-        id: 0,
-        firstName: 'Juku',
-        lastName: 'Juurikas',
-        email: 'juku@juurikas.ee',
-        password: 'juku'
-    },
-    {
-        id: 1,
-        firstName: 'Juhan',
-        lastName: 'Juurikas',
-        email: 'juhan@juurikas.ee',
-        password: 'juhan'
-    }
-];
-
-const usersController = { };
+const usersService = require('../services/usersService');
+const usersController = {};
 
 // Endpoint for getting list of available users
 // GET - users
@@ -23,11 +7,12 @@ const usersController = { };
 // Optional values: none
 // Returns: status 200 - OK and list of users in response body
 usersController.read = (req, res) => {
-  // Return list of users
-  res.status(200).json({
-      success: true,
-      users: users
-  });
+    const users = usersService.read();
+    // Return list of users
+    res.status(200).json({
+        success: true,
+        users: users
+    });
 }
 
 // Endpoint for getting user specified by id
@@ -36,10 +21,12 @@ usersController.read = (req, res) => {
 // Optional: none
 // Returns: status 200 - OK and user data in response body
 usersController.readById = (req, res) => {
+    const userId = req.params.id;
+    const user = usersService.readById(userId);
     // Return user with specified id
     res.status(200).json({
         success: true,
-        user: users[req.params.id]
+        user: user
     });
 }
 
@@ -60,25 +47,18 @@ usersController.create = (req, res) => {
     // Check if required data exists
     if (firstName && lastName && email && password) {
         // Create new json with user data
-        const newUser = {
-            id: users.length,
+        const user = {
             firstName,
             lastName,
             email,
             password
         };
-        // Add user to 'database'
-        users.push(newUser);
 
-        // Create new json from newUser for response
-        const userToReturn = { ... newUser };
-        // Remove password from user data
-        delete userToReturn.password;
-
+        const newUser = usersService.create(user);
         // Return data
         res.status(201).json({
             success: true,
-            user: userToReturn
+            user: newUser
         });
     } else {
         // Return error message
@@ -108,38 +88,28 @@ usersController.update = (req, res) => {
          id = false;
      }
      */
-    const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
-    const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
-    const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
-    const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 3 ? req.body.password : false;
     // Check if required data exists
     if(id || id === 0) {
-        // Check if optional data exists
-        if (firstName) {
-            // Change user data in 'database'
-            users[id].firstName = firstName;
-        }
-        // Check if optional data exists
-        if (lastName) {
-            // Change user data in 'database'
-            users[id].lastName = lastName;
-        }
-        // Check if optional data exists
-        if (email) {
-            // Change user data in 'database'
-            users[id].email = email;
-        }
-        // Check if optional data exists
-        if (password) {
-            // Change user data in 'database'
-            users[id].password = password;
-        }
-        // Return updated user data
-        res.status(200).json({
-            success: true,
-            user: users[id]
-        });
-    } else {
+        const firstName = typeof(req.body.firstName) === 'string' && req.body.firstName.trim().length > 0 ? req.body.firstName : false;
+        const lastName = typeof(req.body.lastName) === 'string' && req.body.lastName.trim().length > 0 ? req.body.lastName : false;
+        const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
+        const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 3 ? req.body.password : false;
+        
+        const user = {
+            id,
+            firstName,
+            lastName,
+            email,
+            password
+        };
+    
+        const updatedUser = usersService.update(user);
+            // Return updated user data
+            res.status(200).json({
+                success: true,
+                user: updatedUser
+            });
+    }  else {
         // Return error message
         res.status(400).json({
             success: false,
---
+++ api/services/usersService.js
@@ -0,0 +1,70 @@
+const users = [
+  {
+      id: 0,
+      firstName: 'Juku',
+      lastName: 'Juurikas',
+      email: 'juku@juurikas.ee',
+      password: 'juku'
+  },
+  {
+      id: 1,
+      firstName: 'Juhan',
+      lastName: 'Juurikas',
+      email: 'juhan@juurikas.ee',
+      password: 'juhan'
+  }
+];
+
+usersService = {};
+
+// Return list of users
+usersService.read = () => {
+  return users;
+}
+
+// Return user by id
+usersService.readById = (userId) => {
+  return users[userId];
+}
+
+// Create user
+usersService.create = (user) => {
+  user.id = users.length;
+  // Add user to 'database'
+  users.push(user);
+
+  // Create new json from newUser for response
+  const userToReturn = { ... user };
+  // Remove password from user data
+  delete userToReturn.password;
+
+  return userToReturn;
+}
+
+usersService.update = (user) => {
+    // Check if optional data exists
+    if (user.firstName) {
+        // Change user data in 'database'
+        users[user.id].firstName = user.firstName;
+    }
+    // Check if optional data exists
+    if (user.lastName) {
+        // Change user data in 'database'
+        users[user.id].lastName = user.lastName;
+    }
+    // Check if optional data exists
+    if (user.email) {
+        // Change user data in 'database'
+        users[user.id].email = user.email;
+    }
+    // Check if optional data exists
+    if (user.password) {
+        // Change user data in 'database'
+        users[user.id].password = user.password;
+    }
+
+    const updatedUser = { ... users[user.id]};
+    delete updatedUser.password;
+    return updatedUser;
+}
+module.exports = usersService;
